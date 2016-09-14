f94f22189fd342100d7f6deb7f53b39f20593ec8
iddings <bryan.iddings@gmail.com>
2016-09-14 16:44:37 -0400

    improved setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+set -v
 wget -O miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
 rm miniconda.sh
-source ~/.bashrc
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
 conda env create
 source activate itvs-venv
