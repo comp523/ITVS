5787cb1c1fdfc03cb497fe1e7f8c8f1b4a5cd658
iddings <bryan.iddings@gmail.com>
2016-09-14 16:37:27 -0400

    travis bugs

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 wget -O miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-sudo bash miniconda.sh -b -p ~/miniconda
+bash miniconda.sh
 rm miniconda.sh
-~/miniconda/bin/conda env create
+source ~/.bashrc
+conda env create
 source activate itvs-venv
