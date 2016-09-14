da752c12ebefd119c65ed0ad68fcf49c6cb23f1c
iddings <bryan.iddings@gmail.com>
2016-09-14 16:27:08 -0400

    make conda silent install

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 wget -O miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-sudo bash miniconda.sh
+sudo bash miniconda.sh -b -p ~/miniconda
 rm miniconda.sh
-source ~/.bashrc
-conda env create
+~/miniconda/bin/conda env create
 source activate itvs-venv
