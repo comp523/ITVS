5b59f4c4b50bce898621bf88e4a52ea98eae0959
iddings <bryan.iddings@gmail.com>
2016-09-14 16:18:55 -0400

    added source command to reload PATH

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@
 wget -O miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sudo bash miniconda.sh
 rm miniconda.sh
+source ~/.bashrc
 conda env create
 source activate itvs-venv
