87610931dde39f5658e33cd8011d22a7bec41d0f
Rourke Creighton <racreigh@localhost.localdomain>
2016-10-16 23:50:20 -0400

    Added basic permalink import functionality

---
+++ ranalyze/ranalyze/imprt.py
@@ -0,0 +1,65 @@
+"""
+Tool to import a set of permalinks from a csv into the database
+"""
+
+import csv
+import praw
+from argparse import SUPPRESS
+from itertools import chain
+from typing import (
+    Callable,
+    Iterable
+)
+from .config import (
+    Config,
+    DictConfigModule
+)
+from .database import Database
+from .scrape import fetch_post
+
+
+class ImportConfig(DictConfigModule):
+    _arguments_dict = {
+        "database": {
+            ("-d", "--database-file"): {
+                "help": ("analysis data will be written "
+                         "to this SQLite file"),
+                "type": str
+            }
+        },
+        "import": {
+            ("-i", "--import"): {
+                "help": "file to import permalinks from",
+                "type": str,
+                "required": True
+            }
+        },
+        "development": {
+            ("--debug",): {
+                "help":SUPPRESS,
+                "dest": "debug",
+                "action": "store_true"
+            }
+        }
+    }
+    def get_runner(self) -> Callable:
+        
+        return main
+
+
+
+def main():
+    """
+    Read in csv file, put permalinks in csv file in the database.
+    """
+
+    config = Config.get_instance()
+
+    database = Database(config["database_file"], config["debug"])
+    
+    with open(config["import"], newline='') as importcsv:
+        csvreader = csv.reader(importcsv)
+        for row in csvreader:
+            for cell in row:
+                for entry in fetch_post(cell):
+                    database.add_update_entry(entry)
