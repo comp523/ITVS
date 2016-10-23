803c25759b696694db0aabcf773033009301a85a
Rourke Creighton <racreigh@localhost.localdomain>
2016-10-23 19:34:30 -0400

    Fix fetch_post and add configuration to __main__ for import functionality

--- ranalyze/ranalyze/__main__.py
+++ ranalyze/ranalyze/__main__.py
@@ -6,6 +6,7 @@ from .config import Config
 from .database import DatabaseConfigModule
 from .scrape import ScrapeConfigModule
 from .search import SearchConfigModule
+from .imprt import ImportConfig
 
 
 def main():
@@ -15,6 +16,7 @@ def main():
     config.register_module(DatabaseConfigModule("create-db"))
     config.register_module(ScrapeConfigModule("scrape"))
     config.register_module(SearchConfigModule("search"))
+    config.register_module(ImportConfig("import"))
 
     runner = config.parse()
 
