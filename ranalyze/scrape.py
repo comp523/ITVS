f149548a1e231c9db8dca7fbcfaab7e116823f96
Bryan Iddings <bryan.iddings@gmail.com>
2017-01-20 13:26:57 -0500

    broke up cron, made scrape/import run hourly, update daily

--- ranalyze/scrape.py
+++ ranalyze/scrape.py
@@ -95,7 +95,7 @@ def fetch_post(permalink):
     # api wrapper connection
     try:
         reddit = praw.Reddit(user_agent="Documenting ecig subs")
-        post = reddit.get_submission(url=permalink)
+        post = reddit.submission(url=permalink)
         post.replace_more_comments(limit=None, threshold=0)
         yield(PostFactory.from_praw(post))
         for comment in traverse_comments(post.comments):
